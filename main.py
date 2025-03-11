6d5839238380f128e54cb1a0a16e68830b3b31b7
menamerai <rphananh@gmail.com>
2025-03-11 14:13:51 -0400

    feat: updated to gymnasium

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import gymnasium as gym
+
+
 def main():
-    print("Hello from adversarial-evolution!")
+    env = gym.make("LunarLander-v3", render_mode="human")
+    observation, info = env.reset()
+
+    episode_over = False
+    while not episode_over:
+        action = (
+            env.action_space.sample()
+        )  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        episode_over = terminated or truncated
+
+    env.close()
 
 
 if __name__ == "__main__":
